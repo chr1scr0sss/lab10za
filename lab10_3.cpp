f4985be59a437020044fdd82416d9365d1543c82
chr1scr0sss <tiwakorn.matonboon@gmail.com>
2024-12-20 13:40:18 +0700

    lab10_3.cpp completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,34 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+        int count = 0;
+        float sum = 0;
+        float sum_of_square = 0;
+        string textline;
+        
+        
+        ifstream source("score.txt");
+        while (getline(source, textline))
+        {
+                sum += atof(textline.c_str());
+                sum_of_square += pow(atof(textline.c_str()), 2);
+                count++;
+        }
+        
+        float mean = sum/count;
+        float var = (sum_of_square/count)-pow(mean, 2);
+        float sd = sqrt(var);
+        
+        cout << "Number of data = " << count << "\n";
+        cout << setprecision(3);
+        cout << "Mean = " << mean << "\n";
+        cout << "Standard deviation = " << sd;
+        
+        return 0;
+}
